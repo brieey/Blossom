233be49c949ca50b5325e1db4945b753bad35695
Brianna <briannanichole247@gmail.com>
2024-11-11 14:43:38 -0600

    Fixed the splash screen to direct to the homepage

---
+++ components/SampleHomePage.js
@@ -0,0 +1,13 @@
+// src/components/HomePage.js
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const SampleHomePage = () => {
+    return (
+        <View>
+            <Text>Welcome to the Home Page!</Text>
+        </View>
+    );
+};
+
+export default SampleHomePage;
